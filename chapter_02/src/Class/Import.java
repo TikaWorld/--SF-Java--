7d1da70dc0311d61a68fcd1eea3cf7e1f4b6d5e3
TikaWorld <tikaworld0416@gmail.com>
2019-03-31 02:46:48 +0900

    Delete Trash file

--- chapter_02/src/Class/Import.java
+++
@@ -1,9 +0,0 @@
-package Class;
-
-import Class.pakage.Pakaging;
-
-public class Import {
-    public static void main(String[] arg) {
-        Pakaging.CallPakage();
-    }
-}
--- chapter_02/src/Class/pakage/Pakaging.java
+++
@@ -1,7 +0,0 @@
-package Class.pakage;
-
-public class Pakaging {
-    public static void CallPakage() {
-        System.out.println("Call Pakage Class.Method!");
-    }
-}
